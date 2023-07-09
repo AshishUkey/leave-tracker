155f2920a44c5cd20304d2bf02478fb49fbfdf9c
Ashish Ukey <ashish.ukey03@gmail.com>
2023-07-10 01:52:49 +0530

    Test

--- force-app/main/default/lwc/leaveTracker/leaveTracker.js
+++ force-app/main/default/lwc/leaveTracker/leaveTracker.js
@@ -4,5 +4,6 @@ import { LightningElement } from 'lwc';
 export default class LeaveTracker extends LightningElement {
     refreshLeaveRequestHandler(event){
         this.refs.leaveRequestComp.refreshGrid();
+        // Test GIT
     }
 }
